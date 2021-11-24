b9ed154605f1d0714552e64d39990f810e0710b6
Noah2211 <noah.decroix3@gmail.com>
2021-11-24 19:29:39 +0100

    exercice done

--- exo.js
+++ exo.js
@@ -1,3 +1,25 @@
 let pomme = "Elle est belle ma pomme !";
+console.log(pomme.length);
+let indexPomme = document.getElementById("index-pomme");
+indexPomme.innerText = pomme.indexOf("pomme");
 
-console.log(pomme.lastIndexOf('m'));
+let lastIndex = document.getElementById("last-index-m");
+lastIndex.innerText = pomme.lastIndexOf("m");
+
+if (pomme.startsWith("Elle")) {
+    console.log("Elle est belle ma pomme ! Commence par Elle");
+}
+
+if (pomme.endsWith("!")) {
+    console.log("Elle est belle ma pomme ! Se termine par un point d'exclamation");
+}
+
+document.getElementById("pomme").innerText = pomme.substring(17, 23);
+
+console.log(pomme [0]);
+
+for (let i = 0; i < pomme.length; i++) {
+    pomme.split("");
+    document.getElementById("one-line").innerText += pomme[i];
+
+}
